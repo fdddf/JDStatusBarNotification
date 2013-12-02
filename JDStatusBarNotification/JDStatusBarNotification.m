a5cd6511233dc777720b6f52e0f7342ebe0011df
Yongliang <viporg@126.com>
2013-12-02 12:25:45 +0800

    Update JDStatusBarNotification.m
    Add a view controller to fix status bar issue

--- JDStatusBarNotification/JDStatusBarNotification.m
+++ JDStatusBarNotification/JDStatusBarNotification.m
@@ -610,3 +610,12 @@ NSString *const JDStatusBarStyleDark    = @"JDStatusBarStyleDark";
 
 @end
 
+/// Add a view controller to fix status bar issue
+@implementation JDViewController
+
+- (UIStatusBarStyle)preferredStatusBarStyle
+{
+	return [UIApplication sharedApplication].statusBarStyle;
+}
+
+@end
